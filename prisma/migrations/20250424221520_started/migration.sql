da0300ceaeecbfcebcfcdf9d43805431b03f511e
asilbekali <asilbekabdugafforov7@gmail.com>
2025-04-25 03:53:08 +0500

    add to authorization and rbac

---
+++ prisma/migrations/20250424221520_started/migration.sql
@@ -0,0 +1,12 @@
+-- CreateEnum
+CREATE TYPE "RoleUsers" AS ENUM ('ADMIN', 'USER');
+
+-- CreateTable
+CREATE TABLE "user" (
+    "id" SERIAL NOT NULL,
+    "name" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+    "role" "RoleUsers" NOT NULL DEFAULT 'USER',
+
+    CONSTRAINT "user_pkey" PRIMARY KEY ("id")
+);
